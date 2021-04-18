c56a58001519571411c395b32ecbd8516f04ac52
Minjin chou <49186771+kuntakinte777@users.noreply.github.com>
2021-04-19 01:52:00 +1000

    View available car page, axios get

---
+++ src/pages/viewcars.tsx
@@ -0,0 +1,52 @@
+import axios, {AxiosResponse} from 'axios'
+import router from 'next/router'
+import { useState } from 'react'
+
+const API_ENDPOINT =
+    'https://ekfj8gcvhh.execute-api.ap-southeast-2.amazonaws.com/test/VehicleAPI'
+
+interface Cars {
+    registration: string;
+    model: string;
+    location_name: number;
+    current_customer: string;
+}
+
+function ViewAvailableCars() {
+    const [errorMsg, setErrorMsg] = useState('')
+
+    async function onSubmit(data: Cars) {
+        const { model, registration, location_name, current_customer  } = data
+        const [cars, setCarList] = useState<Cars[]>([]);
+
+        try {
+            await axios.get<Cars[]>(API_ENDPOINT)
+                .then((response: AxiosResponse) => {
+                    console.log(response.data);
+                    setCarList(response.data);
+
+            });
+        } catch (e) {
+            console.error('Error displaying:', e)
+            setErrorMsg(e.message)
+        }
+    }
+    return (
+        <>
+            <div className="main">
+                <h2>View Available Cars</h2>
+                <div className="col-md-8 col-sm-12">
+                    <div className="view-available-cars">
+                        {errorMsg && (
+                            <div className="alert alert-danger" role="alert">
+                                {errorMsg}
+                            </div>
+                        )}
+                        
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
