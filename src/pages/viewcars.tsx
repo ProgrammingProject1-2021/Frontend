d16da7c695b55ff8374837af11ee79cf96efd7bb
Minjin chou <49186771+kuntakinte777@users.noreply.github.com>
2021-04-20 19:52:05 +1000

    Updated with columngroup

--- src/pages/viewcars.tsx
+++ src/pages/viewcars.tsx
@@ -2,6 +2,8 @@ import axios, {AxiosResponse} from 'axios'
 import {Table, Tag, Space} from 'antd'
 import React, { useState } from 'react'
 
+const {Column, ColumnGroup} = Table;
+
 const API_ENDPOINT =
     'https://ekfj8gcvhh.execute-api.ap-southeast-2.amazonaws.com/test/VehicleAPI'
 
@@ -21,34 +23,47 @@ export default function ViewAvailableCars({cars}: CarsProps) {
         {
             title: 'Model',
             dataIndex: 'Model',
+            key: 'Model',
         },
         {
             title: 'Registration',
             dataIndex: 'Registration',
+            key: 'Registration',
         },
         {
             title: 'Current Customer',
             dataIndex: 'Current_customer',
+            key: 'Current_customer',
         },
         {
             title: 'Location Name',
             dataIndex: 'Location_name',
+            key: 'Location_name',
         },
     ]
 
 
     return (
-        <>
-            <div className="main">
-                <div className="col-md-8 col-sm-12">
+        <div className="container pt-4 pb-3">
+            <div className='row'>
+                <div className="col-md-4">
                     <div className="view-available-cars">
                         <h2>View Available Cars</h2>
-
-                        <Table columns={columns} dataSource={cars} rowKey="id" />
                     </div>
                 </div>
+                <div className="col-md-8">
+                    <Table dataSource={cars}>
+                        <ColumnGroup title="Available cars">
+                            <Column title="Model" dataIndex="Model"/>
+                            <Column title="Registration" dataIndex="Registration"/>
+                            <Column title="Current_customer" dataIndex="Current_customer"/>
+                            <Column title="Location_name" dataIndex="Location_name"/>
+                        </ColumnGroup>
+                    </Table>,
+                </div>
             </div>
-        </>
+        </div>
+
     )
 }
 
