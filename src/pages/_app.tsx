0cf371653affee9d051fceaeb57c8a5f160ee967
north23 <s3679216@student.rmit.edu.au>
2021-05-11 20:22:49 +0700

    ➕ Add nprogress library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.css'
 import 'antd/dist/antd.css'
-
+import 'bootstrap/dist/css/bootstrap.css'
 import App from 'next/app'
 import Head from 'next/head'
+import Router from 'next/router'
+import NProgress from 'nprogress' //nprogress module
+import 'nprogress/nprogress.css' //styles of nprogress
 import React from 'react'
 
 import '../styles/login.css'
+
+//Binding events.
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 class MyApp extends App {
   render() {
@@ -14,11 +21,7 @@ class MyApp extends App {
     return (
       <>
         <Head>
-          <meta
-            key="viewport"
-            name="viewport"
-            content="initial-scale=1.0, width=device-width, shrink-to-fit=no"
-          />
+          <meta key="viewport" name="viewport" content="initial-scale=1.0, width=device-width, shrink-to-fit=no" />
 
           <title>CHH</title>
         </Head>
